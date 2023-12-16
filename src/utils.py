182def8a763267c5464d75f928707bf1f373291d
Ilya Kamens <ilyakamens@gmail.com>
2023-12-16 18:31:21 -0500

    Steal

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,10 @@
+from collections.abc import Iterable, Sequence, deque
+from itertools import islice
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
 def chunks(list, size):
     for i in range(0, len(list), size):
         yield list[i : i + size]
@@ -9,3 +16,20 @@ def rotateleft(matrix):
 
 def rotateright(matrix):
     return [list(l) for l in list(zip(*matrix[::-1]))]
+
+
+# Taken from Zak.
+def flip_rows_cols(lines: Sequence[str]) -> list[str]:
+    return ["".join(x).strip() for x in zip(*lines)]
+
+
+# Taken from Zak.
+def sliding_window(iterable: Iterable[T], n: int) -> Iterable[Iterable[T]]:
+    """sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG."""
+    it = iter(iterable)
+    window = deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
