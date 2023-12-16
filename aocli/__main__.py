f87e004b49c3b645f5ed31a50b667e052454023b
Ilya Kamens <ilyakamens@gmail.com>
2023-12-16 17:59:40 -0500

    Incorporate examples

--- aocli/__main__.py
+++ aocli/__main__.py
@@ -19,13 +19,16 @@ args = parser.parse_args()
 
 data = get_data(year=args.year, day=int(args.day))
 
-input_day_path = SRC / "input" / args.year / args.day
-inputpath = str(input_day_path / "input.txt")
-Path(str(input_day_path)).mkdir(parents=True, exist_ok=True)
-with open(inputpath, "w") as f:
+root_input_path = SRC / "input" / args.year / args.day
+input_path = str(root_input_path / "input.txt")
+Path(str(root_input_path)).mkdir(parents=True, exist_ok=True)
+with open(input_path, "w") as f:
     f.write(data)
+example_path = str(root_input_path / "example-1.txt")
+if not os.path.exists(example_path):
+    open(example_path, "w").close()
 
 daypath = str(SRC / args.year / args.day) + ".py"
 if not os.path.exists(daypath):
     call([str(ROOT / "mkday.sh"), args.year, args.day])
-call([daypath, args.year, args.day, inputpath])
+call([daypath, args.year, args.day, root_input_path])
--- src/2023/16.py
+++ src/2023/16.py
@@ -5,6 +5,7 @@
 from collections import defaultdict
 import copy
 from dataclasses import dataclass
+import os
 import sys
 
 import aocd
@@ -90,8 +91,23 @@ def p2(lines):
 if __name__ == "__main__":
     year, day, path = sys.argv[1:]
 
-    with open(path) as f:
+    i = 1
+    while True:
+        example_path = path + f"/example-{i}.txt"
+        if not os.path.exists(example_path):
+            break
+        with open(example_path) as f:
+            lines = [list(l) for l in f.read().splitlines()]
+        i += 1
+
+    print("Examples:")
+    print(f"Part a: {p1(lines)}")
+    print(f"Part b: {p2(lines)}")
+    print()
+
+    with open(path + "/input.txt") as f:
         lines = [list(l) for l in f.read().splitlines()]
 
+    print("Real:")
     aocd.submit(p1(lines), part="a", day=int(day), year=int(year))
     aocd.submit(p2(lines), part="b", day=int(day), year=int(year))
