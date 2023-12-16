efd05407dd201f2f7b41f95b4e4917c39e9ee206
Ilya Kamens <ilyakamens@gmail.com>
2023-12-16 17:02:34 -0500

    Add to utils

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,11 @@
 def chunks(list, size):
     for i in range(0, len(list), size):
         yield list[i : i + size]
+
+
+def rotateleft(matrix):
+    return [list(l) for l in list(zip(*matrix))[::-1]]
+
+
+def rotateright(matrix):
+    return [list(l) for l in list(zip(*matrix[::-1]))]
