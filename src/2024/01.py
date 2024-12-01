dba6bed2fc60d30a25cf39ac1db75fb682de7b47
Ilya Kamens <ilyakamens@gmail.com>
2024-12-01 17:13:16 +0800

    Day 1

--- src/2024/01.py
+++ src/2024/01.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python
 
-"""https://adventofcode.com/2024/day/01."""
+"""https://adventofcode.com/2024/day/1."""
 
 from main import main
 
 
-def p1(lines):
-    pass
+def parse_input(input):
+    l1, l2 = [], []
+    for line in input.splitlines():
+        l, r = line.split()
+        l1.append(int(l))
+        l2.append(int(r))
+    return l1, l2
 
 
-def p2(lines):
-    pass
+def p1(input):
+    l1, l2 = parse_input(input)
+
+    l1.sort()
+    l2.sort()
+
+    return sum(abs(v2 - v1) for v1, v2 in zip(l1, l2))
 
 
-if __name__ == "__main__":
-    main(p1, p2, [], [])
+def p2(input):
+    l1, l2 = parse_input(input)
+
+    return sum(v * l2.count(v) for v in l1)
+
+
+if __name__ == '__main__':
+    main(p1, p2, [11], [31])
