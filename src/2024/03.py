dd4c831e53f8c42be1f26a4006f21155ae7a538b
Ilya Kamens <ilyakamens@gmail.com>
2024-12-03 14:25:26 +0800

    More Day 3 cleanup

--- src/2024/03.py
+++ src/2024/03.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-"""https://adventofcode.com/2024/day/3."""
+"""https://adventofcode.com/2024/day/3.
+
+Cleaned-up solution inspired by zakj's.
+"""
 
 import re
 
@@ -10,45 +13,26 @@ mul_re = r'mul\((\d{1,3}),(\d{1,3})\)'
 
 
 def p1(input):
-    sums = 0
-    for match in re.findall(mul_re, input):
-        sums += int(match[0]) * int(match[1])
+    matches = re.findall(mul_re, input)
 
-    return sums
+    return sum(int(m[0]) * int(m[1]) for m in matches)
 
 
 def p2(input):
-    sums = 0
-    do = True
-    i = 0
-    do_re = re.compile(r'do\(\)')
-    dont_re = re.compile(r"don't\(\)")
+    all_re = r'|'.join([mul_re, r'do\(\)', r"don't\(\)"])
+    matches = [m for m in re.finditer(all_re, input)]
 
-    while i < len(input):
-        if do_re.match(input[i : i + 4]):
-            i += 4
-            do = True
-            continue
-        if dont_re.match(input[i : i + 7]):
-            i += 7
-            do = False
-            continue
-        if not do:
-            do_i = input[i:].find('do()')
-            if do_i == -1:
-                break
-            i += do_i
-            continue
+    total = 0
+    enabled = True
+    for m in matches:
+        if m.group() == 'do()':
+            enabled = True
+        elif m.group() == "don't()":
+            enabled = False
+        elif enabled:
+            total += int(m.group(1)) * int(m.group(2))
 
-        match = re.match(mul_re, input[i : i + 12])
-        if match:
-            sums += int(match.group(1)) * int(match.group(2))
-            i += match.end()
-            continue
-
-        i += 1
-
-    return sums
+    return total
 
 
 if __name__ == '__main__':
