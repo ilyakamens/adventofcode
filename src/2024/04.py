216c002b2920161c2b885ce3dc301ed1ed0892b5
Ilya Kamens <ilyakamens@gmail.com>
2024-12-04 14:27:50 +0800

    Day 4, 2024 cleanup + grid

--- src/2024/04.py
+++ src/2024/04.py
@@ -2,97 +2,37 @@
 
 """https://adventofcode.com/2024/day/4."""
 
-from collections import defaultdict
 from typing import Annotated
 
 from main import main
+from utils import Dir8, DirDiag, Grid
 
 run_type = Annotated[str, '1, 2, 3, etc., or real']
 
 
-def check_square(m, i, j):
-    count = 0
-    if m[i][j] + m[i + 1][j] + m[i + 2][j] + m[i + 3][j] == 'XMAS':
-        count += 1
-    if m[i][j] + m[i - 1][j] + m[i - 2][j] + m[i - 3][j] == 'XMAS':
-        count += 1
-    if m[i][j] + m[i][j + 1] + m[i][j + 2] + m[i][j + 3] == 'XMAS':
-        count += 1
-    if m[i][j] + m[i][j - 1] + m[i][j - 2] + m[i][j - 3] == 'XMAS':
-        count += 1
-    if m[i][j] + m[i + 1][j + 1] + m[i + 2][j + 2] + m[i + 3][j + 3] == 'XMAS':
-        count += 1
-    if m[i][j] + m[i + 1][j - 1] + m[i + 2][j - 2] + m[i + 3][j - 3] == 'XMAS':
-        count += 1
-    if m[i][j] + m[i - 1][j - 1] + m[i - 2][j - 2] + m[i - 3][j - 3] == 'XMAS':
-        count += 1
-    if m[i][j] + m[i - 1][j + 1] + m[i - 2][j + 2] + m[i - 3][j + 3] == 'XMAS':
-        count += 1
-
-    return count
-
-
 def p1(run: run_type, input: str) -> int:
-    m = defaultdict(lambda: defaultdict(str))
-    for i, line in enumerate(input.splitlines()):
-        for j, c in enumerate(line):
-            m[i][j] = c
+    g = Grid(input)
 
     count = 0
-    for i in range(len(m) + 1):
-        for j in range(len(m[i]) + 1):
-            count += check_square(m, i, j)
+    for x, y in g.iter():
+        for dir in Dir8.iter():
+            if g.substr(x, y, dir, 4) == 'XMAS':
+                count += 1
 
     return count
 
 
 def p2(run: run_type, input: str) -> int:
-    m = defaultdict(lambda: defaultdict(str))
-    for i, line in enumerate(input.splitlines()):
-        for j, c in enumerate(line):
-            m[i][j] = c
+    g = Grid(input)
 
     count = 0
-    for i in range(len(m)):
-        for j in range(len(m[i])):
-            count += check2(m, i, j)
+    for x, y in g.iter():
+        if all(
+            g.substr(x, y, dir, 3, offset=-1) in {'MAS', 'SAM'} for dir in [DirDiag.NE, DirDiag.SE]
+        ):
+            count += 1
 
     return count
-
-
-def check2(m, i, j):
-    if m[i][j] != 'A':
-        return 0
-    if (
-        m[i + 1][j + 1] == 'M'
-        and m[i + 1][j - 1] == 'M'
-        and m[i - 1][j - 1] == 'S'
-        and m[i - 1][j + 1] == 'S'
-    ):
-        return 1
-    if (
-        m[i + 1][j - 1] == 'M'
-        and m[i - 1][j - 1] == 'M'
-        and m[i - 1][j + 1] == 'S'
-        and m[i + 1][j + 1] == 'S'
-    ):
-        return 1
-    if (
-        m[i - 1][j - 1] == 'M'
-        and m[i - 1][j + 1] == 'M'
-        and m[i + 1][j + 1] == 'S'
-        and m[i + 1][j - 1] == 'S'
-    ):
-        return 1
-    if (
-        m[i - 1][j + 1] == 'M'
-        and m[i + 1][j + 1] == 'M'
-        and m[i + 1][j - 1] == 'S'
-        and m[i - 1][j - 1] == 'S'
-    ):
-        return 1
-
-    return 0
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
-import collections
+from collections import defaultdict, deque
 from collections.abc import Iterable
 from itertools import islice
-from typing import TypeVar
+from typing import Iterator, TypeVar
 
 T = TypeVar('T')
+
+Vector = tuple[int, int]
 
 
 def numbers(line: str) -> list[int]:
@@ -36,13 +38,73 @@ def flip_rows_cols(lines: list[str] | list[list[str]] | list[list[int]]) -> list
     return flipped
 
 
-# Taken from Zak.
+# Taken from zakj.
 def sliding_window(iterable: Iterable[T], n: int) -> Iterable[Iterable[T]]:
     """sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG."""
     it = iter(iterable)
-    window = collections.deque(islice(it, n), maxlen=n)
+    window = deque(islice(it, n), maxlen=n)
     if len(window) == n:
         yield tuple(window)
     for x in it:
         window.append(x)
         yield tuple(window)
+
+
+# Inspired by zakj's.
+class Dir:
+    N: Vector = (0, -1)
+    E: Vector = (1, 0)
+    S: Vector = (0, 1)
+    W: Vector = (-1, 0)
+
+    @classmethod
+    def iter(cls) -> Iterator[Vector]:
+        for d in [cls.N, cls.E, cls.S, cls.W]:
+            yield d
+
+
+# Inspired by zakj's.
+class DirDiag:
+    NE: Vector = (1, -1)
+    SE: Vector = (1, 1)
+    SW: Vector = (-1, 1)
+    NW: Vector = (-1, -1)
+
+    @classmethod
+    def iter(cls) -> Iterator[Vector]:
+        for d in [cls.NE, cls.SE, cls.SW, cls.NW]:
+            yield d
+
+
+# Inspired by zakj's.
+class Dir8(Dir, DirDiag):
+    @classmethod
+    def iter(cls) -> Iterator[Vector]:
+        for d in [cls.N, cls.NE, cls.E, cls.SE, cls.S, cls.SW, cls.W, cls.NW]:
+            yield d
+
+
+class Grid:
+    def __init__(self, input: str, t: str | int = str):
+        self.m = defaultdict(lambda: defaultdict(t))
+
+        for y, line in enumerate(input.splitlines()):
+            for x, c in enumerate(line):
+                self.m[x][y] = c if t is str else int(c)
+
+    def __getitem__(self, x: int) -> dict[int, T]:
+        return self.m[x]
+
+    def iter(self):
+        for y in range(len(self.m)):
+            for x in range(len(self.m[y])):
+                yield x, y
+
+    def substr(self, x: int, y: int, dir: Dir | DirDiag | Dir8, length: int, offset=0):
+        dirx, diry = dir
+
+        s = ''
+        for i in range(length):
+            s += self[x + (i * dirx) + (offset * dirx)][y + (i * diry) + (offset * diry)]
+
+        return s
