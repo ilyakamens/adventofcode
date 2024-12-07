60239081b512fbd562cfb105d3cb244d1855e685
Ilya Kamens <ilyakamens@gmail.com>
2024-12-07 23:34:47 +0800

    Day 7, 2024 cleanup

--- src/2024/07.py
+++ src/2024/07.py
@@ -3,57 +3,63 @@
 """https://adventofcode.com/2024/day/7."""
 
 from itertools import product
+from typing import Callable, Generator
 
 from main import main, runs
 from utils import numbers
 
 
+def add(l, r):
+    return l + r
+
+
+def mult(l, r):
+    return l * r
+
+
+def concat(l, r):
+    return int(str(l) + str(r))
+
+
+def iter_equations(
+    input: str, ops: list[Callable]
+) -> Generator[int, list[int], list[list[Callable]]]:
+    for line in input.splitlines():
+        test_val, *nums = numbers(line)
+        yield test_val, nums, product(ops, repeat=len(nums) - 1)
+
+
+def has_match(test_val: int, nums: list[int], opss: list[Callable]) -> bool:
+    for ops in opss:
+        total = nums[0]
+        for num, op in zip(nums[1:], ops):
+            total = op(total, num)
+            if total > test_val:
+                break
+        if total == test_val:
+            return True
+
+    return False
+
+
 @runs(cases={'1'})
 def p1(input: str, case: str) -> int:
-    vals = []
-    for line in input.splitlines():
-        res, *rest = numbers(line)
-        opss = product('*+', repeat=len(rest) - 1)
-        for ops in opss:
-            ops = list(ops)
-            nums = list(rest.copy())
-            total = nums.pop(0)
-            while nums:
-                op = ops.pop(0)
-                num = nums.pop(0)
-                total = eval(f'{total}{op}{num}')
-            if total == int(res):
-                vals.append(res)
-                break
+    total = 0
+    for test_val, nums, opss in iter_equations(input, ops=[add, mult]):
+        if has_match(test_val, nums, opss):
+            total += test_val
 
-    return sum(vals)
+    return total
 
 
 @runs(cases={'1'})
 def p2(input: str, case: str) -> int:
-    vals = []
-    for line in input.splitlines():
-        print(line)
-        res, *rest = numbers(line)
-        opss = product('*+|', repeat=len(rest) - 1)
-        for ops in opss:
-            ops = list(ops)
-            nums = list(rest.copy())
-            total = nums.pop(0)
-            while nums:
-                op = ops.pop(0)
-                num = nums.pop(0)
-                if op == '|':
-                    total = int(str(total) + str(num))
-                else:
-                    total = eval(f'{total}{op}{num}')
-                if total > res:
-                    break
-            if total == res:
-                vals.append(res)
-                break
+    total = 0
+    for test_val, nums, opss in iter_equations(input, ops=[add, mult, concat]):
+        if has_match(test_val, nums, opss):
+            total += test_val
 
-    return sum(vals)
+    return total
 
 
 if __name__ == '__main__':
