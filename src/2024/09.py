2b29cd90d514fc7f9fa3105be27c275a3fccde55
Ilya Kamens <ilyakamens@gmail.com>
2024-12-09 21:35:17 +0800

    Day 9, 2024 (non-working) cleanup

--- src/2024/09.py
+++ src/2024/09.py
@@ -5,32 +5,23 @@
 from dataclasses import dataclass
 
 from main import main, runs
+from utils import LL, Node
 
 
 @runs(cases={'1'})
 def p1(input: str, case: str) -> int:
     input = input.strip()
 
-    count = 0
-    for c in input:
-        count += int(c)
-
     disk = []
-    file = True
-    filecount = 0
-    for c in input:
-        for _ in range(int(c)):
-            if file:
-                disk.append(str(filecount))
-            else:
-                disk.append('.')
-        if file:
-            filecount += 1
-        file = not file
+    fileid = 0
+    for i, c in enumerate(input):
+        is_file = i % 2 == 0
+        disk.extend([str(fileid) if is_file else '.'] * int(c))
+        fileid += 1 if is_file else 0
 
     i = 0
     j = len(disk) - 1
-    while i < j:
+    while True:
         while disk[i] != '.':
             i += 1
         while disk[j] == '.':
@@ -41,74 +32,85 @@ def p1(input: str, case: str) -> int:
         i += 1
         j -= 1
 
-    size = 0
-    for i, c in enumerate(disk):
-        if c == '.':
-            break
-        size += i * int(c)
-
-    return size
+    return sum(i * int(c) for i, c in enumerate(disk) if c != '.')
 
 
 @dataclass
 class File:
     size: int
     id: int = -1
+    moved: bool = False
+
+    @property
+    def is_file(self) -> bool:
+        return self.id != -1
+
+    @property
+    def is_block(self) -> bool:
+        return self.id == -1
+
+
+@dataclass
+class Disk:
+    items: LL[Node[File]]
+
+    @property
+    def checksum(self) -> int:
+        checksum = 0
+
+        i = 0
+        f: File
+        for f in self.items.itervals():
+            if f.is_block:
+                i += f.size
+                continue
+
+            for _ in range(f.size):
+                checksum += i * f.id
+                i += 1
+
+        return checksum
+
+    def move(self, from_: Node[File], to: Node[File]):
+        file, block = from_.val, to.val
+
+        remaining = block.size - file.size
+        block.size = file.size
+        block.id, file.id = file.id, block.id
+        block.moved = True
+
+        if remaining > 0:
+            self.items.insert(File(size=remaining), after=to)
 
 
 @runs(cases={'1'})
 def p2(input: str, case: str) -> int:
     input = input.strip()
 
-    count = 0
-    for c in input:
-        count += int(c)
-
-    disk = []
-    file = True
+    items = LL[Node[File]]()
     fileid = 0
-    for c in input:
-        if file:
-            disk.append(File(size=int(c), id=fileid))
-            fileid += 1
-        else:
-            disk.append(File(size=int(c)))
-        file = not file
+    for i, c in enumerate(input):
+        is_file = i % 2 == 0
+        items.append(File(size=int(c), id=fileid if is_file else -1))
+        fileid += 1 if is_file else 0
 
-    i = 0
-    j = len(disk) - 1
-    remaining = 0
-    for j in range(len(disk) - 1, 0, -1):
-        f1 = disk[j]
-        if f1.id == -1:
+    disk = Disk(items)
+
+    for i, filenode in reversed(list(enumerate(disk.items))):
+        file = filenode.val
+        if not file.is_file or file.moved:
             continue
-        for i, f2 in enumerate(disk):
-            if i >= j:
+        for j, blocknode in enumerate(disk.items):
+            if j >= i:
                 break
-            if f2.id != -1:
+            block = blocknode.val
+            if not block.is_block:
                 continue
-            if f1.size <= f2.size:
-                remaining = f2.size - f1.size
-                f1.id, f2.id = f2.id, f1.id
-                f2.size = f1.size
+            if block.size >= file.size:
+                disk.move(filenode, blocknode)
                 break
 
-        if remaining > 0:
-            disk.insert(i + 1, File(size=remaining))
-            remaining = 0
-
-    size = 0
-    i = 0
-    for f in disk:
-        if f.id == -1:
-            i += f.size
-            continue
-
-        for _ in range(f.size):
-            size += i * f.id
-            i += 1
-
-    return size
+    return disk.checksum
 
 
 if __name__ == '__main__':
