bbca9e28addc7354bb1b31b59a1761e28f17347b
Ilya Kamens <ilyakamens@gmail.com>
2024-12-17 07:37:17 +0800

    Improve output + add runtimes

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 from collections.abc import Callable
 from functools import wraps
 from typing import Annotated
@@ -30,12 +31,13 @@ def _print_rich(p1_pairs, p2_pairs):
     with Live(table_centered, console=console, screen=False, refresh_per_second=20):
         table.add_column('Part', justify='center', style='cyan', no_wrap=True)
         table.add_column('Result', justify='center', no_wrap=True)
+        table.add_column('Time', justify='center', no_wrap=True)
 
-        for p1_mine, p1_their in p1_pairs:
-            table.add_row('A', _format_result(p1_mine, p1_their))
+        for (p1_mine, p1_time), p1_their in p1_pairs:
+            table.add_row('A', _format_result(p1_mine, p1_their), f'{p1_time}ms')
 
-        for p2_mine, p2_their in p2_pairs:
-            table.add_row('B', _format_result(p2_mine, p2_their))
+        for (p2_mine, p2_time), p2_their in p2_pairs:
+            table.add_row('B', _format_result(p2_mine, p2_their), f'{p2_time}ms')
 
     return table
 
@@ -54,24 +56,29 @@ def main(
             break
         with open(example_path) as f:
             example_input = f.read()
-        p1_mines.append(p1(str(i), example_input))
-        p2_mines.append(p2(str(i), example_input))
+        p1_mines.append(timeit(lambda: p1(str(i), example_input)))
+        p2_mines.append(timeit(lambda: p2(str(i), example_input)))
         i += 1
-
-    _print_rich(zip(p1_mines, p1_theirs), zip(p2_mines, p2_theirs))
 
     with open(path + '/input.txt') as f:
         input = f.read()
 
     print('Real:')
-    if p1_theirs == p1_mines:
-        aocd.submit(p1('real', input), part='a', day=int(day), year=int(year))
+    if [p[0] for p in p1_mines] == p1_theirs:
+        p1_answer, p1_duration = timeit(lambda: p1('real', input))
+        aocd.submit(p1_answer, part='a', day=int(day), year=int(year))
     else:
-        print('(Part a skipped.)')
-    if p2_theirs == p2_mines:
-        aocd.submit(p2('real', input), part='b', day=int(day), year=int(year))
+        p1_answer, p1_duration = 'Skipped', ''
+    if [p[0] for p in p2_mines] == p2_theirs:
+        p2_answer, p2_duration = timeit(lambda: p2('real', input))
+        aocd.submit(p2_answer, part='b', day=int(day), year=int(year))
     else:
-        print('(Part b skipped.)')
+        p2_answer, p2_duration = 'Skipped', ''
+    p1_mines.append((p1_answer, p1_duration))
+    p2_mines.append((p2_answer, p2_duration))
+    p1_theirs.append(None)
+    p2_theirs.append(None)
+    _print_rich(zip(p1_mines, p1_theirs), zip(p2_mines, p2_theirs))
 
 
 def runs(cases: Annotated[set[str], '(Example input) 1, 2, 3, etc.']):
@@ -83,3 +90,11 @@ def runs(cases: Annotated[set[str], '(Example input) 1, 2, 3, etc.']):
         return wrapper
 
     return inner
+
+
+def timeit(f):
+    start = time.perf_counter()
+    result = f()
+    end = time.perf_counter()
+
+    return result, f'{(end - start) * 1000:.2f}'
